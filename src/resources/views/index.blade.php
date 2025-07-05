305de274dc6a1daf1db61cfd52ab50e9bb774adc
gotouge <gotouge@gmail.com>
2025-07-05 10:07:40 +0900

    topページ修正 金額と商品名

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -15,12 +15,8 @@
         <div class="card__content">
             <div class="card__cat">カテゴリー</div>
             <h2 class="card__ttl">
-                今日の朝ごはんは卵と肉を合わせたバランスの良いメニューです。
+                商品名
             </h2>
-            <div class="tag">
-                <p class="card__tag">#朝ごはん</p>
-                <p class="card__date">2021/01/01</p>
-            </div>
         </div>
     </div>
     <div class="practice__card">
@@ -30,13 +26,9 @@
             </div>
         </a>
         <div class="card__content">
-            <div class="card__cat">カテゴリー</div>
+            <div class="card__cat">¥500</div>
             <h2 class="card__ttl">
-                今日の朝ごはんは卵と肉を合わせたバランスの良いメニューです。 </h2>
-            <div class="tag">
-                <p class="card__tag">#朝ごはん</p>
-                <p class="card__date">2021/01/01</p>
-            </div>
+                商品名 </h2>
         </div>
     </div>
     <div class="practice__card">
