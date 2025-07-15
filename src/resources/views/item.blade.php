baa36fa782ae98115e8da9a8fa9d236b5e5361f8
gotouge <gotouge@gmail.com>
2025-07-16 07:31:12 +0900

    商品詳細レイアウト調整 注文完了画面作成

--- src/resources/views/item.blade.php
+++ src/resources/views/item.blade.php
@@ -11,15 +11,20 @@
             <img src="{{ asset('storage/img/card.jpg')}}" />
         </div>
         <div class="item-content">
-            <div class="item__ttl">テストだよ</div>
-            <div class="item__price">¥1000</div>
-            <div class="item__price--sub">（税込）送料込み</div>
+            <div class="item__ttl">
+                <h2>テストだよ</h2>
+            </div>
+            <div class="item__price">¥1000（税込）送料込み</div>
             <a href="/item">
-                <div class="buy">商品を購入する</div>
+                <div class="item_buy">商品を購入する</div>
             </a>
             <div class="item-explanation">
-                <h2>商品の説明</h2>
+                <h3>商品の説明</h3>
                 <p>test</p>
+            </div>
+            <div class="seller">
+                <h3>出品者</h3>
+                <p>出品者のプロフィールアイコン</p>
             </div>
         </div>
     </div>
