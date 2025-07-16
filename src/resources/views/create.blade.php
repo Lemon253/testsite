3697194dd06a62be2d93f4bc8cedc9e2fa5c4869
gotouge <gotouge@gmail.com>
2025-07-17 07:21:06 +0900

    出品画面のレイアウト調整

--- src/resources/views/create.blade.php
+++ src/resources/views/create.blade.php
@@ -5,13 +5,24 @@
 @endsection
 
 @section('content')
-<div>
+<div class="create-form">
     <form method="POST" action="{{ route('store') }}" enctype="multipart/form-data">
         @csrf
-        <input type="text" name="name">
-        <input type="number" name="price">
-        <input type="file" name="image_url">
-        <input type="submit" value="登録">
+        <div class="create-form__name">
+            <p>商品名：</p>
+            <input type="text" name="name">
+        </div>
+        <div class="create-form__price">
+            <p>値段：</p>
+            <input type="number" name="price">
+        </div>
+        <div class="create-form__image">
+            <p>商品画像：</p>
+            <input type="file" name="image_url">
+        </div>
+        <div class="create-form__button">
+            <input type="submit" value="登録">
+        </div>
     </form>
 </div>
 @endsection
